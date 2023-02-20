6bafc70f847f8ba93ab1e3f57e69366e61b2b3c0
saidmmendez <70990998+saidmmendez@users.noreply.github.com>
2023-02-19 19:45:35 -0700

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Enzo Mendez on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
